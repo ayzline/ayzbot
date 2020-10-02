8e274cb3cc8a3eba939b9774db5948caeedf79c0
ayzline <52289643+ayzline@users.noreply.github.com>
2020-10-02 17:50:43 +0700

    Update ayzbot.py

--- ayzbot.py
+++ ayzbot.py
@@ -6,7 +6,7 @@ from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
 from bs4 import BeautifulSoup
 from threading import Thread
-from googletrans import Translator
+from google trans import Translator
 from gtts import gTTS
 import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,os,subprocess,requests,tempfile
 
